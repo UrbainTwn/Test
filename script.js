8f3dc2a2b92f447de127eda62cc2187eb5b277b9
UrbainTwn <162590594+UrbainTwn@users.noreply.github.com>
2024-10-10 15:02:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let texts = [
     { texte: "1 Corinthiens 10:11", sous_texte: "Ces choses leur sont arrivées pour servir d’exemples, et elles ont été écrites pour notre instruction, à nous qui sommes parvenus à la fin des siècles." },
     { texte: "2 Timothée 2:20-21", sous_texte: "Dans une grande maison, il n’y a pas seulement des vases d’or et d’argent, mais il y en a aussi de bois et de terre; les uns sont des vases d’honneur, et les autres sont d’un usage vil. Si donc quelqu’un se conserve pur, en s’abstenant de ces choses, il sera un vase d’honneur, sanctifié, utile à son maître, propre à toute bonne œuvre." },
     { texte: "Acte 9:15", sous_texte: "Mais le Seigneur lui dit: Va, car cet homme est un instrument que j’ai choisi, pour porter mon nom devant les nations, devant les rois, et devant les fils d’Israël." },
-        
+    { texte: "proverbes 24:12", sous_texte: "Si tu dis: Ah! Nous ne savions pas!… Celui qui pèse les cœurs ne le voit-il pas? Celui qui veille sur ton âme ne le connaît-il pas? Et ne rendra-t-il pas à chacun selon ses œuvres?" },
         ];
 
 // Sauvegarder les données dans le LocalStorage
